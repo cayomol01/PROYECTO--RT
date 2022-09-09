aa55ea76152527a3cb6144e7b642794d0106ac14
cayomol01 <jacmg01@gmail.com>
2022-09-08 19:08:50 -0600

    nose

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ grass = Material(diffuse = (0.3, 1, 0.3))
 
 snow = Material(diffuse = (1,1,1))
 button = Material(diffuse = (0.2,0.2,0.2))
+mouth = Material(diffuse=(0.4,0.4,0.4))
+nose = Material(diffuse = (1,0.63,0.20))
 
 
 
@@ -28,13 +30,26 @@ rtx.scene.append( Sphere(V3(-4,-2,-15), 1.5, stone)  )
 rtx.scene.append( Sphere(V3(2,2,-8), 0.2, grass)  )
  '''
 
-
+#snowballs
 rtx.scene.append(Sphere(V3(0,-2,-10), 2.5, snow))
 rtx.scene.append(Sphere(V3(0,1,-10), 2, snow))
 rtx.scene.append(Sphere(V3(0,3.5,-10), 1.5, snow))
+
+#buttons
 rtx.scene.append(Sphere(V3(0,-1.5,-7.5), 0.40, button))
 rtx.scene.append(Sphere(V3(0,0,-8), 0.30, button))
 rtx.scene.append(Sphere(V3(0,1.5,-8), 0.30, button))
+
+#mouth
+rtx.scene.append(Sphere(V3(-0.5,2.9,-8.75), 0.125, mouth))
+rtx.scene.append(Sphere(V3(0.5,2.9,-8.75), 0.125, mouth))
+rtx.scene.append(Sphere(V3(0.20,2.8,-8.75), 0.125, mouth))
+rtx.scene.append(Sphere(V3(-0.20,2.8,-8.75), 0.125, mouth))
+
+
+#Nose
+rtx.scene.append(Sphere(V3(0,3.5,-8.50), 0.20, nose))
+
 rtx.glRender()
 
 rtx.glFinish("outputs/output.bmp")
